e1488abb06b1c0bed185eb89803491a3748b71af
Brett Hageft <brett.hageft@gmail.com>
2022-01-25 17:56:44 -0700

    added to splash page

--- react-app/src/components/Splash/index.js
+++ react-app/src/components/Splash/index.js
@@ -9,6 +9,8 @@ const Splash = () => {
             <div className="splash-container">
                 <div className="splash-header">
                     <h1> Mercury Hearth</h1>
+                    <p>Mercury Hearth is a tool for the creation of worlds and everything within them.</p>
+            
                     <img alt='' src='https://images.unsplash.com/photo-1642606440166-2bd57032ea92?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxlZGl0b3JpYWwtZmVlZHwxOXx8fGVufDB8fHx8'></img>
                 </div>
             </div>
