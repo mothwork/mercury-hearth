1c819393112ea45c53b773e249baf171405c3a34
Brett Hageft <brett.hageft@gmail.com>
2022-01-27 16:09:05 -0700

    Fixed about link

--- react-app/src/components/Splash/index.js
+++ react-app/src/components/Splash/index.js
@@ -10,7 +10,7 @@ const Splash = () => {
                 <div className="splash-header">
                     <h1> Mercury Hearth</h1>
                     <p className='about-line'>Mercury Hearth is a tool for the  <a href='https://en.wikipedia.org/wiki/Worldbuilding'>creation of worlds</a> and everything within them.</p>
-                    <p className='about-line'>Built by Brett Hageft <a  href='https://github.com/mothwork'>github</a>::<a href='www.linkedin.com/in/mothwork'>LinkedIn</a></p>
+                    <p className='about-line'>Built by Brett Hageft <a  href='https://github.com/mothwork'>github</a>::<a href='https://www.linkedin.com/in/mothwork'>LinkedIn</a></p>
                     <img alt='' src='https://images.unsplash.com/photo-1642606440166-2bd57032ea92?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxlZGl0b3JpYWwtZmVlZHwxOXx8fGVufDB8fHx8'></img>
                 </div>
             </div>
