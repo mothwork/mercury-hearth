e5f3c9fb5f711b0e4679eab00ae3cd8dd1b4cdef
Brett Hageft <brett.hageft@gmail.com>
2022-05-23 16:13:21 -0700

    splash changes

--- react-app/src/components/Splash/index.js
+++ react-app/src/components/Splash/index.js
@@ -23,23 +23,21 @@ const Splash = () => {
                 <div className="splash-header">
                 <div className='login-links'>
                     <ul>
-                        <li>
+                        <li className='login-link'>
                             <LoginFormModal />
                         </li>
-                        <li>
+                        <li className='login-link'>
                             <SignUpModal />
                         </li>
-                        <li>
+                        <li className='long-link'>
                             <button className='nav-buttons' onClick={demoLogin}>Demo Now</button>
                         </li>
                     </ul>
                 </div>
-                    <div className='img-div'>
-                        <img alt="" className='img-circle' src='https://images.unsplash.com/photo-1589807867695-d541586cd70c?crop=entropy&cs=tinysrgb&fm=jpg&ixlib=rb-1.2.1'></img>
+                    <div className='title-group'>
+                    <h1 className='splash-title'>Mercury Hearth</h1>
+                    <p className='about-line'>Create expansive worlds for your TTRPG, fiction, and video game projects.</p>
                     </div>
-                    <h1>Mercury Hearth</h1>
-                    <p className='about-line'>Mercury Hearth is a worldbuilding application that allows you to create expansive worlds for your TTRPG, fiction, and video game projects.</p>
-
                     <p className='about-line'>Built by Brett Hageft <a target='_blank' rel='noreferrer' href='https://github.com/mothwork'>github</a>::<a rel='noreferrer' target='_blank' href='https://www.linkedin.com/in/mothwork'>LinkedIn</a></p>
 
                 </div>
